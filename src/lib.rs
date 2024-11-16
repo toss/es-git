8a7c589b776004c555659eaccd53611a6ec85f05
Seokju Na <seokju.me@gmail.com>
2024-11-16 18:59:53 +0900

    using error with `thiserror`

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,10 @@ extern crate napi_derive;
 
 pub mod branch;
 pub mod context;
+mod error;
 pub mod reference;
 pub mod remote;
 pub mod repository;
 pub mod tag;
+
+pub type Result<T> = std::result::Result<T, error::Error>;
