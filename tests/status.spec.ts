45885a0968f278699210b159744663f36c57e8a7
Seokju Na <seokju.me@gmail.com>
2025-04-19 22:04:00 +0900

    win32

--- tests/status.spec.ts
+++ tests/status.spec.ts
@@ -2,10 +2,11 @@ import { writeFile } from 'node:fs/promises';
 import path from 'node:path';
 import { describe, expect, it } from 'vitest';
 import { openRepository } from '../index';
+import { isTarget } from './env';
 import { useFixture } from './fixtures';
 
 describe('status', () => {
-  it('get status entries', async () => {
+  it('get status entries', { skip: isTarget('win32') }, async () => {
     const p = await useFixture('commits');
     const repo = await openRepository(p);
     await writeFile(path.join(p, 'added'), 'added', 'utf8');
@@ -17,6 +18,7 @@ describe('status', () => {
     index.write();
 
     const statuses = repo.statuses();
+    expect(statuses.len()).toBe(3n);
     const entries = [...statuses.iter()];
     expect(entries).toHaveLength(3);
     const getEntry = (path: string) => entries.find(x => x.path() === path)!;
@@ -40,6 +42,28 @@ describe('status', () => {
         indexNew: false,
         wtNew: false,
         wtModified: true,
+      })
+    );
+  });
+
+  it('get status entries (for win32)', { skip: !isTarget('win32') }, async () => {
+    const p = await useFixture('empty');
+    const repo = await openRepository(p);
+    await writeFile(path.join(p, 'added'), 'added', 'utf8');
+    const index = repo.index();
+    index.addPath('added');
+    index.write();
+
+    const statuses = repo.statuses();
+    expect(statuses.len()).toBe(1n);
+    const entries = [...statuses.iter()];
+    expect(entries).toHaveLength(1);
+    const getEntry = (path: string) => entries.find(x => x.path() === path)!;
+    expect(getEntry('added').status()).toEqual(
+      expect.objectContaining({
+        current: true,
+        indexNew: true,
+        wtNew: false,
       })
     );
   });
