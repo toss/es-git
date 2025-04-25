70031c87be45c06ce152300c04ae0da7388f2890
Sanggyu Kang <other.yuka@gmail.com>
2025-04-26 01:02:36 +0900

    refactor(bench): pre-initialize repositories for accurate benchmarks

--- benchmarks/util.ts
+++ benchmarks/util.ts
@@ -1,6 +1,10 @@
 import { exec as execChildProcess } from 'node:child_process';
+import { Repository as SimpleGitRepository } from '@napi-rs/simple-git';
+import { Repository as NodeGitRepository } from 'nodegit';
+import { type BenchOptions, bench } from 'vitest';
+import { type Repository, openRepository } from '../index';
 
-export function exec(command: string, cwd: string) {
+export function exec(command: string, cwd: string): Promise<string> {
   return new Promise<string>((resolve, reject) => {
     let output = '';
     const cp = execChildProcess(
@@ -19,4 +23,86 @@ export function exec(command: string, cwd: string) {
     );
     cp.on('close', () => resolve(output));
   });
+}
+
+export function createBenchmark(gitDir: string) {
+  const benchESGit = (
+    name: string,
+    operation: (repo: Repository) => unknown | Promise<unknown>,
+    options?: BenchOptions
+  ) => {
+    let esGitRepo: Repository;
+    bench(
+      name,
+      async () => {
+        if (!esGitRepo) throw new Error('Setup failed for es-git benchmark');
+        await operation(esGitRepo);
+      },
+      {
+        setup: async () => {
+          esGitRepo = await openRepository(gitDir);
+        },
+        ...options,
+      }
+    );
+  };
+
+  const benchNodeGit = (
+    name: string,
+    operation: (repo: NodeGitRepository) => unknown | Promise<unknown>,
+    options?: BenchOptions
+  ) => {
+    let nodeGitRepo: NodeGitRepository;
+    bench(
+      name,
+      async () => {
+        if (!nodeGitRepo) throw new Error('Setup failed for nodegit benchmark');
+        await operation(nodeGitRepo);
+      },
+      {
+        setup: async () => {
+          nodeGitRepo = await NodeGitRepository.open(gitDir);
+        },
+        ...options,
+      }
+    );
+  };
+
+  const benchSimpleGit = (
+    name: string,
+    operation: (repo: SimpleGitRepository) => unknown | Promise<unknown>,
+    options?: BenchOptions
+  ) => {
+    let simpleGitRepo: SimpleGitRepository;
+    bench(
+      name,
+      () => {
+        if (!simpleGitRepo) throw new Error('Setup failed for simple-git benchmark');
+        operation(simpleGitRepo);
+      },
+      {
+        setup: () => {
+          simpleGitRepo = new SimpleGitRepository(gitDir);
+        },
+        ...options,
+      }
+    );
+  };
+
+  const benchChildProcess = (name: string, operation: () => unknown | Promise<unknown>, options?: BenchOptions) => {
+    bench(
+      name,
+      async () => {
+        await operation();
+      },
+      options
+    );
+  };
+
+  return {
+    esGit: benchESGit,
+    nodegit: benchNodeGit,
+    simpleGit: benchSimpleGit,
+    childProcess: benchChildProcess,
+  };
 }
