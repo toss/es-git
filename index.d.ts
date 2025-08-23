ecc5ce304be01b0343fa97287aac703894b96f88
seokju-na <seokju.me@toss.im>
2025-08-23 21:48:30 +0900

    fix: jsdoc unmatched parameters (#147)

--- index.d.ts
+++ index.d.ts
@@ -4850,7 +4850,7 @@ export declare class Repository {
    * }
    * ```
    *
-   * @param {Commit} outCommit - The commit that reflects the destination tree.
+   * @param {Commit} ourCommit - The commit that reflects the destination tree.
    * @param {Commit} theirCommit - The commit to merge in to `ourCommit`.
    * @param {MergeOptions} [options] - Merge options.
    * @returns The index result.
@@ -4876,7 +4876,7 @@ export declare class Repository {
    * ```
    *
    * @param {Tree} ancestorTree - The common ancestor between.
-   * @param {Tree} outTree - The tree that reflects the destination tree.
+   * @param {Tree} ourTree - The tree that reflects the destination tree.
    * @param {Tree} theirTree - The tree to merge in to `ourTree`.
    * @param {MergeOptions} [options] - Merge options.
    * @returns The index result.
@@ -5242,7 +5242,7 @@ export declare class Repository {
    * }
    * ```
    *
-   * @param {Commit} commitish - A Commit which the HEAD should point to.
+   * @param {Commit} commit - A Commit which the HEAD should point to.
    */
   setHeadDetached(commit: Commit): void
   /**
